ef1623fa0fa70a9b295214ed78740361894074d7
gurbur <jihwan7577@gmail.com>
2025-05-18 14:05:13 +0900

    Feat: project initialization for oms

---
+++ apps/oms/src/test/java/com/jihwan/logistics/oms/OmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jihwan.logistics.oms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
